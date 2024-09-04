2024135c7f18e624897e05d2db319cbf26f8ad0e
Yuliya100996 <zwfggg@gmail.com>
2024-09-05 00:51:56 +0500

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,7 @@
+for i in range(3, 21):
+    string = ''
+    for j in range(1, i):
+        for k in range(j+1, i):
+            if i % (j+k) == 0:
+                string += str(j) + str(k)
+    print(f'{i} - {string}')
