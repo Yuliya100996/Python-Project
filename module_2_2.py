165f549a5f7671b5a860c9ec01f49ca28f771776
Yuliya100996 <zwfggg@gmail.com>
2024-09-05 00:53:22 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,19 @@
+first = 9
+second = 15
+third = 9
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
+
+first = 7
+second = 15
+third = 9
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
