66a3376db80ad16cc8beea21f4f55935ad9ded73
Yuliya100996 <zwfggg@gmail.com>
2024-09-12 23:39:27 +0500

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -17,7 +17,11 @@ class House:
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors != self.number_of_floors
+        if isinstance(other, House):
+            self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            self.number_of_floors == other
+        return self
 
     def __add__(self, value):
         if isinstance(value, House):
@@ -27,12 +31,45 @@ class House:
         return self
 
     def __radd__(self, value):
-        #return self.number_of_floors + value
         return self.__add__(value)
 
     def __iadd__(self, value):
-        #return self.number_of_floors + value
         return self.__add__(value)
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            self.number_of_floors < other
+        return self
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            self.number_of_floors <= other
+        return self
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            self.number_of_floors > other
+        return self
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            self.number_of_floors >= other
+        return self
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            self.number_of_floors != other
+        return self
 
 
 h1 = House('ЖК Эльбрус', 10)
