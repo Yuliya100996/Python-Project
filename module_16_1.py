6fcb987cdcb8481913de0780121ec9c61ed54e9d
Yuliya100996 <zwfggg@gmail.com>
2025-01-15 16:23:06 +0500

    Add files via upload
    Основы Fast Api и маршрутизация.

---
+++ module_16_1.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root() -> str:
+    return "Главная страница"
+
+@app.get("/user/admin")
+async def get_admin() -> str:
+    return "Вы вошли как администратор"
+
+@app.get("/user/{user_id}")
+async def get_user(user_id: str) -> str:
+    return "Вы вошли как пользователь № " + user_id
+
+@app.get("/user")
+async def get_user_data(username: str, age: str) -> str:
+    return f"Информация о пользователе. Имя: 'Юлия', Возраст: 28."
